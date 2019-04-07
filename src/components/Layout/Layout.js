3feaceb027a8ef26366d0385ab2f3e20aee61fb7
Angel Gomez <jangelgr7@gmail.com>
2019-04-07 00:25:26 -0400

    chore: add burgerbuilder component and layout basic css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import classes from './Layout.module.css';
+
 const layout = (props) => (
   <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.content}>
       {props.children}
     </main>
   </>
