a7a4c003eb4f39854ab50fdcd2e0da1dd9a260e9
Angel Gomez <jangelgr7@gmail.com>
2019-05-18 16:22:49 -0400

    chore: save and fetch orders by user id

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import * as actions from '../../store/actions';
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders(this.props.token);
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
 
   render () {
@@ -35,11 +35,12 @@ const mapStateToProps = state => ({
   orders: state.order.orders,
   loading: state.order.loading,
   token: state.auth.token,
+  userId: state.auth.userId,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onFetchOrders(token) {
-    dispatch(actions.fetchOrders(token));
+  onFetchOrders(token, userId) {
+    dispatch(actions.fetchOrders(token, userId));
   },
 });
 
