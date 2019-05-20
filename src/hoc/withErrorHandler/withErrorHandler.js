a7df36226433bdc9dca80907007ca235e5421953
Angel Gomez <jangelgr7@gmail.com>
2019-05-20 18:18:05 -0400

    feat: use custom hook for http error handling

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return props => {
-    const [error, setError] = useState(null);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    }
-
-    const reqInterceptor = axios.interceptors.request.use(req => {
-      setError(null);
-      return req;
-    });
-
-    const resInterceptor = axios.interceptors.response.use(res => res, err => {
-      setError(err);
-    });
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.response.eject(resInterceptor);
-      }
-    }, [reqInterceptor, resInterceptor]);
+    const [error, clearError] = useHttpErrorHandler(axios);
 
     return (
       <>
         <Modal
           show={error}
-          modalClosed={errorConfirmedHandler}
+          modalClosed={clearError}
         >
           {error ? error.message : null}
         </Modal>
